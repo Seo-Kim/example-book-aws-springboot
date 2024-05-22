36c51e54b6336e6f4a8a258ba3bef42341e5fb5a
SeoK <l0vest0ry@paran.com>
2024-05-23 05:11:50 +0900

    JPA, Entity 추가

---
+++ src/main/java/com/seok/example/book_aws/domain/posts/Post.java
@@ -0,0 +1,34 @@
+package com.seok.example.book_aws.domain.posts;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter  // 수정 시 명확한 의도를 반영하기 위해 Setter 없이 직접 메서드 작성
+@EqualsAndHashCode( of="id" )
+@NoArgsConstructor
+@Entity
+public class Post {  // camel case로 작성 시 snake case로 연결
+    @Id
+    // H2: "insert into post (author,content,title,id) values (?,?,?,default)"
+    // MySQL: "insert into post (author,content,title) values (?,?,?)"
+    @GeneratedValue( strategy=GenerationType.IDENTITY )  // IDENTITY = auto increment
+    private Long id;
+
+    @Column( length=500, nullable=false )  // length default = 255
+    private String title;
+
+    @Column( columnDefinition="TEXT", nullable=false )  // type default = VARCHAR
+    private String content;
+
+    private String author;
+
+    @Builder  // 빌더 생성 (lombok)
+    public Post( String title, String content, String author ) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/test/java/com/seok/example/book_aws/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.seok.example.book_aws.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith( SpringExtension.class )
+@SpringBootTest  // H2 DB 실행 목적
+class PostRepositoryTest {
+    @Autowired
+    PostRepository postRepository;
+
+    @AfterEach  // Each: 각 테스트 메서드 실행 후 매번 실행, All: 모든 테스트 메서드 실행 후 한 번 실행 (반드시 static)
+    public void cleanUp() {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 저장하고_불러오기() {
+        String title = "제목 테스트";
+        String content = "본문 내용 테스트";
+        String author = "admin@seok.com";
+        Post reqPost = Post.builder()
+                .title( title )
+                .content( content )
+                .author( author )
+                .build();
+
+        postRepository.save( reqPost );
+        List< Post > postList = postRepository.findAll();
+
+        Post resPost = postList.get( 0 );
+        assertEquals( title, resPost.getTitle() );
+        assertEquals( content, resPost.getContent() );
+        assertEquals( reqPost, resPost );  // equals() 정의로 동일 id는 같은 객체로 봄
+    }
+}
