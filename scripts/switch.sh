40fdd4c7440af28d961951778f022a2d5021db72
SeoK <l0vest0ry@paran.com>
2024-07-04 06:18:01 +0900

    sudo need a password

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -8,9 +8,11 @@ function switch_proxy() {
     IDLE_PORT=$( find_idle_port )
     echo '>> rewrite url to config file :: '"${IDLE_PORT}"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" \
-      | sudo tee '/etc/nginx/conf.d/service-url.inc'
+      | cat > '/etc/nginx/conf.d/service-url.inc'
+#      | sudo tee '/etc/nginx/conf.d/service-url.inc'
 
     echo '>> NginX reload'
-    sudo nginx -t
-    sudo nginx -s reload
+#    sudo nginx -t
+#    sudo nginx -s reload
+    service nginx reload
 }
