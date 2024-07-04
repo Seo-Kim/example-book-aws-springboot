2034508e31e14fa12b35e31b9ed8cfdb12410efe
SeoK <l0vest0ry@paran.com>
2024-07-04 14:19:42 +0900

    nginx reload 실행 불가 > sudo nopasswd 설정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,11 +9,16 @@ function switch_proxy() {
     echo '>> rewrite url to config file :: '"${IDLE_PORT}"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" \
       | cat > "/etc/nginx/conf.d/service-url.inc"
-#      | sudo tee '/etc/nginx/conf.d/service-url.inc'
+    #  | sudo tee '/etc/nginx/conf.d/service-url.inc'
         ## tee:
+        ## sudo password required
 
     echo '>> NginX reload'
-#    sudo nginx -t
-#    sudo nginx -s reload
-    service nginx reload
+    #sudo nginx -t
+    sudo nginx -s reload
+      ## sudo password required
+    #service nginx reload
+      ## subsystem request failed on channel 0 < ssh -t -s "sudo"
+      ## Failed to relaod nginx.service: Interactive authentication required.
+    #systemctl reload nginx
 }
